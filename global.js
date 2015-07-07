f5243678814862f394d32d456dfb20760c1f8e9c
Nicholas Emanuel <njemanuel01@gmail.com>
2015-07-07 15:33:04 -0500

    Added loop to quiz questions

--- global.js
+++ global.js
@@ -4,20 +4,21 @@ var question3 = {question:"When did Nebraska become a state?", choices:"(1)-1867
 var question4 = {question:"What is Nebraska's largest city?", choices:"(1)-Omaha, (2)-Lincoln, (3)-Kearney, (4)-Fremont", answer: 1};
 var question5 = {question:"What is the largest county in Nebraska?", choices:"(1)-Sarpy, (2)-Dodge, (3)-Cherry, (4)-Doublas", answer: 3};
 var question_array = [question1, question2, question3, question4, question5];
+var score = 0, count = 0;
 
-
-var question = document.getElementById("questions");
-question.innerText = question1.question;
-
-var choices = document.getElementById("choices");
-choices.innerText = question1.choices;
+document.getElementById("questions").innerText = question_array[count].question; 
+document.getElementById("choices").innerText = question_array[count].choices;
+  
+document.getElementById("submitter").onclick = process_answer_submission;
+  
+document.getElementById("next").onclick = next_question;
 
 function given_answer(){
   return document.getElementById("answer").value;
 }
 
 function is_correct_answer(answer_text){
-  if (answer_text == question1.answer) {
+  if (answer_text == question_array[count].answer) {
     return true;
   } 
   return false;
@@ -25,9 +26,10 @@ function is_correct_answer(answer_text){
 
 function update_question_result(correct){
   if (correct) {
-    document.getElementById("question_result").innerText = "Correct"
+    score++;
+    document.getElementById("question_result").innerText = "Correct";
   } else {
-    document.getElementById("question_result").innerText = "Incorrect"
+    document.getElementById("question_result").innerText = "Incorrect";
   }
 }
 
@@ -35,3 +37,17 @@ function process_answer_submission(){
   var user_answer = given_answer();
   update_question_result(is_correct_answer(user_answer));
 }
+
+function next_question(){
+  count++
+  if (count < 5 ) {
+    document.getElementById("questions").innerText = question_array[count].question; 
+    document.getElementById("choices").innerText = question_array[count].choices;
+    document.getElementById("answer").value = null;
+    document.getElementById("question_result").innerText = "";
+  } else {
+    percentage = ((score/5.0) * 100);
+    document.getElementById("total_result").innerText = "All done. Your score was: " + percentage + "%.";
+  }
+}
+
